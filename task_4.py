0d61e705697cc81022c66b4a1181f0c5053ab8a4
ivan <ivan.pavlov.alk@gmail.com>
2021-03-25 17:36:40 +0200

    Tasks completed

--- task_4.py
+++ task_4.py
@@ -1,12 +1,14 @@
 import random
 
+
 class Car:
-    def __init__(self):
+    def __init__(self, ispolice=False):
         color_list = ("red", "green", "blue", "white", "black")
+        model_list = ("Ford", "Dodge", "Plimouth", "Chevrolet", "GMC", "Shelby")
         self.speed = random.randint(1, 120)
         self.color = random.choice(color_list)
-        self.name = input("Enter car model: ")
-        self.ispolice = True
+        self.name = random.choice(model_list)
+        self.ispolice = ispolice
 
     def car_motion_go(self):
         print(f"{self.name} {self.color} is drive with {self.speed} m/h")
@@ -24,25 +26,49 @@ class Car:
 class TownCar(Car):
     def car_motion_go(self):
         if self.speed > 60:
-            print(f"{self.name} {self.color} is moving at an overspeed. This is Town Car")
+            print(f"{self.name} {self.color} is moving at an overspeed {self.speed}. This is Town Car")
+        else:
+            print(f"{self.name} {self.color} is drive with {self.speed} m/h")
 
 
 class WorkCar(Car):
-    super().car_motion_left()
-
     def car_motion_go(self):
         if self.speed > 20:
-            print(f"{self.name} {self.color} is moving at an overspeed. This is Work Car")
+            print(f"{self.name} {self.color} is moving at an overspeed {self.speed}. This is Work Car")
+        else:
+            print(f"{self.name} {self.color} is drive with {self.speed} m/h")
 
 
 class PoliceCar(Car):
-   if super(Car).ispolice() is True:
-       super(Car).name = f" {super(Car).name} (police car)"
+    def __init__(self, ispolice=True):
+        super().__init__(ispolice)
+        self.name = f" {self.name} *047* NYPD"
 
 
-car1 = PoliceCar()
-car1.car_motion_go()
-car1.car_motion_left()
-car1.car_motion_right()
-car1.car_motion_stop()
-car1.police_car_color()
+class SportCar(Car):
+    pass
+
+
+pd_car = PoliceCar()
+pd_car.car_motion_go()
+pd_car.car_motion_left()
+pd_car.car_motion_right()
+pd_car.car_motion_stop()
+print("*"*50)
+tw_car = TownCar()
+tw_car.car_motion_go()
+tw_car.car_motion_left()
+tw_car.car_motion_right()
+tw_car.car_motion_stop()
+print("*"*50)
+w_car = WorkCar()
+w_car.car_motion_go()
+w_car.car_motion_left()
+w_car.car_motion_right()
+w_car.car_motion_stop()
+print("*"*50)
+s_car = SportCar()
+s_car.car_motion_go()
+s_car.car_motion_left()
+s_car.car_motion_right()
+s_car.car_motion_stop()
